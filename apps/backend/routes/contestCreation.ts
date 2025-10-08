41b141d34e3be580f59ebfc0183d9258ce24468d
MANEPALLI HARI <manepallihari@MANEPALLIs-MacBook-Air.local>
2025-10-09 00:04:09 +0530

    added create and edit challenge routes

--- apps/backend/routes/contestCreation.ts
+++ apps/backend/routes/contestCreation.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createContestBody, editContestDurationBody, editContestStartTimeBody, editContestTitleBody } from "../types";
+import { createChallengeBody, createContestBody, editChallengeContestBody, editChallengeMaxPointsBody, editChallengeNotionDocIdBody, editChallengeTitleBody, editContestDurationBody, editContestStartTimeBody, editContestTitleBody } from "../types";
 import { client } from "db/client";
 import { da } from "zod/locales";
 
@@ -9,121 +9,398 @@ const r=Router();
 
 r.post("/contest",async (req,res)=>{
 
-    const {success,data,error}= createContestBody.safeParse(req.body);
+               const {success,data,error}= createContestBody.safeParse(req.body);
 
-    if(!success){
-        res
-        .status(411)
-        .json({
-            message :"Input validation failed"
-        })
-        return;
-    }
-    if(!data){
-        res
-        .status(400)
-        .json({
-            message: "Bad request"
-        })
-    }
-     
+               if(!success){
+                   res
+                   .status(411)
+                   .json({
+                       message :"Input validation failed"
+                   })
+                   return;
+               }
+               if(!data){
+                   res
+                   .status(400)
+                   .json({
+                       message: "Bad request"
+                   })
+               }
 
-    const contest= await client.contest.create({
-        data:{
-            title: data.title,
-            startTime: data.startTime,
-            Duration: data.duration
-
-        }
-    })
-
-    res
-    .json({
-        message: `Contest named ${data.title} created`
-    })
+           
+             try {
+                 const contest= await client.contest.create({
+                     data:{
+                         title: data.title,
+                         startTime: data.startTime,
+                         Duration: data.duration
+                    
+                     }
+                 })
+             
+                 res
+                 .json({
+                     message: `Contest named ${data.title} created`
+                 })
+             } catch (error) {
+                       res
+                       .status(500)
+                       .json({
+                        message:" Something went wrong.."
+                       })
+    
+  }
 
 })
 
 r.post("/editContestTitle", async(req,res)=>{
 
-    const {success,data}= editContestTitleBody.safeParse(req.body);
+                   const {success,data}= editContestTitleBody.safeParse(req.body);
+                
+                   if(!success){
+                       res
+                       .status(411)
+                       .json({
+                           message: "Input validation failed"
+                       })
+                       return;
+                   }
+               
+                  try {
+                    const contest=await client.contest.update({
+                        where:{
+                            id:data.id
+                        },
+                        data:{
+                             title:data.title
+                        }
+                    })
+                
+                    res
+                    .json({
+                        message: "Contest title updated successfully"
+                    })
+                  } catch (error) {
+                   res
+                   .status(500)
+                   .json({
+                    message:" Something went wrong.."
+                   })
+                   
+                  }
+   
+})
+
+r.post("/editContestStartTime", async (req, res)=>{
+
+              const {success,data}= editContestStartTimeBody.safeParse(req.body);
+              if(!success){
+                  res
+                  .status(411)
+                  .json({
+                      message:" Input validation failed"
+                  })
+                  return;
+              }
+          
+             try {
+               const contest= await client.contest.update({
+                   where:{
+                       id:data.id
+                   },
+                   data:{
+                       startTime: data.startTime
+                   }
+               })
+           
+               res
+               .json({
+                   message:`Contest ${contest.title} start time updated successfully`
+               })
+             } catch (error) {
+                res
+                .status(500)
+                .json({
+                 message:" Something went wrong.."
+                })
+             }
+})
+
+r.post("/editContestDuration", async (req,res)=>{
+    const {success,data}= editContestDurationBody.safeParse(req.body);
+
+                if(!success){
+                    res
+                    .status(411)
+                    .json({
+                        message:"Input Validation failed"
+                    })
+                    return;
+                }
+
+                try {
+                      const contest= await client.contest.update({
+                          where:{
+                              id:data.id
+                          },
+                          data:{
+                              Duration: data.duration
+                          }
+                      })
+                      res.
+                      json({
+                          message: `Contest ${contest.title} duration updated successfully`
+                      })
+                } catch (error) {
+                   res
+                   .status(500)
+                   .json({
+                    message:" Something went wrong.."
+                   })
+
+ }
+})
+
+r.post("/createChallenge", async (req,res)=>{
+
+
+    const {success,data} = createChallengeBody.safeParse(req.body);
+
+            if(!success){
+                res
+                .status(411)
+                .json({
+                    message:"Input validation failed"
+                })
+                return;
+            }
+
+            try {
+                  const challenge= await client.challenge.create({
+                      data:{
+                          title:data.title,
+                          notionDocId: data.notionDocId,
+                          maxPoints: data.maxPoints
+                      }
+                  })
+              
+                  const contesttoChallengeMapping = await client.contestToChallengeMapping.create({
+                      data:{
+                          contestId: data.contestId,
+                          challengeId:challenge.id,
+                          index: 0
+                      }
+                  })
+
+
+                        res
+                        .json({
+                          message:`Challenge ${challenge.title} created successfully`
+                        })
+
+            } catch (error) {
+            
+               res
+               .status(500)
+               .json({
+                message:" Something went wrong.."
+               })
+            
+            }
+})
+
+r.post("/editChallengeTitle", async (req,res)=>{
+
+
+    const {success, data}= editChallengeTitleBody.safeParse(req.body);
 
     if(!success){
+          res
+          .status(411)
+          .json({
+            message :"Input validation failed"
+          })
+          return;
+    }
+
+    try {
+         
+        const challenge= await client.challenge.update({
+            where:{
+                id: data.id
+            },
+            data:{
+                title: data.title
+            }
+        })
+
+        // const contestToChallenge = await client.contestToChallengeMapping.updateMany({
+        //     where:{
+        //         challengeId: data.id
+        //     },
+        //     data:{
+        //         challengeId:challenge.id
+        //     }
+        // })
+
+
+        res
+        .json({
+            message:"Challenge title updated successfully.."
+        })
+        
+    } catch (error) {
+        res
+        .status(500)
+        .json({
+         message:" Something went wrong.."
+        })
+        
+    }
+    
+})
+
+r.post("/editChallengeNotionDocId", async (req,res)=>{
+
+    const {success,data} = editChallengeNotionDocIdBody.safeParse(req.body);
+
+    if(!success){
+
         res
         .status(411)
         .json({
             message: "Input validation failed"
         })
         return;
+
     }
 
-    const contest=await client.contest.update({
-        where:{
-            id:data.id
-        },
-        data:{
-             title:data.title
-        }
-    })
+    try {
 
-    res
-    .json({
-        message: "Contest title updated successfully"
-    })
-   
+
+        const challenge= await client.challenge.update({
+            where: {
+                id: data.id
+            },
+            data:{
+                notionDocId: data.notionDocId
+            }
+        })
+
+        res.json({
+            message:`Notion doc of ${challenge.title} updated successfully`
+        })
+        
+    } catch (error) {
+          
+        res
+        .status(500)
+        .json({
+         message:" Something went wrong.."
+        })
+        
+        
+    }
+
 })
 
-r.post("/editContestStartTime", async (req, res)=>{
+r.post("/editChallengeMaxpoints", async (req,res)=>{
 
-    const {success,data}= editContestStartTimeBody.safeParse(req.body);
+
+    const {success,data}= editChallengeMaxPointsBody.safeParse(req.body);
     if(!success){
+
         res
         .status(411)
         .json({
-            message:" Input validation failed"
+            message: "Input validation failed"
         })
         return;
+
     }
 
-    const contest= await client.contest.update({
-        where:{
-            id:data.id
-        },
-        data:{
-            startTime: data.startTime
-        }
-    })
+    try {
 
-    res
-    .json({
-        message:`Contest ${contest.title} start time updated successfully`
-    })
+        const challenge= await client.challenge.update({
+            where:{
+                id: data.id
+            },
+            data:{
+                maxPoints: data.maxPoints
+            }
+        })
+        
+
+        res
+        .json({
+            message:`Challenge ${challenge.title} maxpoints updated...`
+        })
+    } catch (error) {
+        res
+        .status(500)
+        .json({
+         message:" Something went wrong.."
+        })
+        
+    }
 })
 
-r.post("/editContestDuration", async (req,res)=>{
-    const {success,data}= editContestDurationBody.safeParse(req.body);
 
+r.post("/editChallengeContest", async (req, res)=>{
+
+    const {success,data}= editChallengeContestBody.safeParse(req.body);
     if(!success){
+
         res
         .status(411)
         .json({
-            message:"Input Validation failed"
+            message: "Input validation failed"
         })
         return;
-    }
 
-    const contest= await client.contest.update({
-        where:{
-            id:data.id
+    }
+    try {
+
+       const challengeToContestMapping= await client.contestToChallengeMapping.update({
+        where: {
+            id: data.contestToChallengeMappingId
         },
         data:{
-            Duration: data.duration
+            contestId:data.contestId
+        }
+       })
+
+       res
+       .json({
+        message: "Contest-Challenge Mapping changed successfully..."
+       })
+    } catch (error) {
+
+        res
+        .status(500)
+        .json({
+         message:" Something went wrong.."
+        })
+
+        
+    }
+
+})
+
+r.post("/check", async (req,res)=>{
+
+    const pp= await client.contestToChallengeMapping.findMany({
+        where:{
+
+        },
+        include:{
+            challenge:true,
+            contest:true
         }
     })
-    res.
-    json({
-        message: `Contest ${contest.title} duration updated successfully`
-    })
+  
+    res
+    .json(pp)
 })
 
 
--- apps/backend/types.ts
+++ apps/backend/types.ts
@@ -26,3 +26,32 @@ export const editContestDurationBody =z.object({
     id:z.string(),
     duration:z.number()
 })
+
+export const createChallengeBody= z.object({
+    contestId: z.string(),
+    title: z.string(),
+    notionDocId : z.string(),
+    maxPoints: z.number()
+})
+
+
+export const editChallengeTitleBody =z.object({
+    id:z.string(),
+    title: z.string()
+})
+
+export const editChallengeNotionDocIdBody = z.object({
+    id: z.string(),
+    notionDocId: z.string()
+})
+
+export const editChallengeMaxPointsBody= z.object({
+    id: z.string(),
+    maxPoints: z.number()
+})
+
+export const editChallengeContestBody= z.object({
+  
+    contestId: z.string(),
+    contestToChallengeMappingId: z.string()
+})
